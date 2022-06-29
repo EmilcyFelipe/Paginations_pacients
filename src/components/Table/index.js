5b6f93d1b21ff9d1a331fce21215c0e83076bceb
Felipe de Paula <emilcyfelipe@gmail.com>
2022-06-29 10:22:08 -0300

    first commit

---
+++ src/components/Table/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Container } from "./styles";
+export default function Table() {
+  return (
+    <Container>
+      <h3>tabela de produtos</h3>
+    </Container>
+  );
+}
